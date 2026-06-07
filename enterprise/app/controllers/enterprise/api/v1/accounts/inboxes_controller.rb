f74240c94718586c4e35e468cdef82e56fcfc946
md-riaz <mdriazwd@gmail.com>
2026-06-07 12:47:34 +0600

    fix: route voice inboxes through voice channel

--- enterprise/app/controllers/enterprise/api/v1/accounts/inboxes_controller.rb
+++ enterprise/app/controllers/enterprise/api/v1/accounts/inboxes_controller.rb
@@ -40,19 +40,21 @@ module Enterprise::Api::V1::Accounts::InboxesController
   end
 
   def channel_type_from_params
-    return Channel::TwilioSms if permitted_params[:channel][:type] == 'voice'
+    return Channel::Voice if permitted_params[:channel][:type] == 'voice'
 
     super
   end
 
   def account_channels_method
-    return Current.account.twilio_sms if permitted_params[:channel][:type] == 'voice'
+    return Current.account.voice_channels if permitted_params[:channel][:type] == 'voice'
 
     super
   end
 
   def create_channel
-    return create_voice_channel if permitted_params[:channel][:type] == 'voice'
+    if permitted_params[:channel][:type] == 'voice'
+      raise Pundit::NotAuthorizedError unless Current.account.feature_enabled?('channel_voice')
+    end
 
     super
   end
@@ -61,25 +63,5 @@ module Enterprise::Api::V1::Accounts::InboxesController
     attrs = super
     attrs += [:voice_enabled, :api_key_sid, :api_key_secret] if channel_type == 'Channel::TwilioSms' && @inbox&.channel&.medium == 'sms'
     attrs
-  end
-
-  def create_voice_channel
-    raise Pundit::NotAuthorizedError unless Current.account.feature_enabled?('channel_voice')
-
-    voice_params = params.require(:channel).permit(
-      :phone_number, :provider,
-      provider_config: [:account_sid, :auth_token, :api_key_sid, :api_key_secret]
-    )
-    config = voice_params[:provider_config] || {}
-
-    Current.account.twilio_sms.create!(
-      phone_number: voice_params[:phone_number],
-      account_sid: config[:account_sid],
-      auth_token: config[:auth_token],
-      api_key_sid: config[:api_key_sid],
-      api_key_secret: config[:api_key_secret],
-      medium: :sms,
-      voice_enabled: true
-    )
   end
 end
